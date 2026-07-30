c809309c88ac9274e86a2e22e3bf426bcdc30c54
Arlo O'Keeffe <tiger@k5d.de>
2026-07-30 21:35:43 +0200

    update the mac index to show no changes

--- checkout.sh
+++ checkout.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 git clone https://github.com/mozilla/multi-account-containers mac
 git clone https://github.com/GodKratos/temporary-containers tcp
+git update-index --assume-unchanged ./mac/src/img/webicon-*
